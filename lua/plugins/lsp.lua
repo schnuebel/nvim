0be71bae64455a2b9e8815642f00ef6e71dc1b3f
Leonard <leonard.ruebel@gmail.com>
2025-04-12 15:18:32 +0200

    goimports und gomfuysdmfsd

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -153,7 +153,6 @@ return {
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       -- clangd = {},
-      gopls = {},
       -- pyright = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -212,7 +211,43 @@ return {
           },
         },
       },
+      gopls = {
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+            },
+            staticcheck = true,
+            gofumpt = true,
+          },
+        },
+      },
+      goimports = {},
+      gofumpt = {},
     }
+
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      pattern = '*.go',
+      callback = function()
+        local params = vim.lsp.util.make_range_params()
+        params.context = { only = { 'source.organizeImports' } }
+        -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+        -- machine and codebase, you may want longer. Add an additional
+        -- argument after params if you find that you have to write the file
+        -- twice for changes to be saved.
+        -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+        local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params)
+        for cid, res in pairs(result or {}) do
+          for _, r in pairs(res.result or {}) do
+            if r.edit then
+              local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or 'utf-16'
+              vim.lsp.util.apply_workspace_edit(r.edit, enc)
+            end
+          end
+        end
+        vim.lsp.buf.format { async = false }
+      end,
+    })
 
     -- Ensure the servers and tools above are installed
     --  To check the current status of installed tools and/or manually install
